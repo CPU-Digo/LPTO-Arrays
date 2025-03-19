2f755fed23092afc32a9868c108e21770ef9eaa5
CPU-Digo <168138711+CPU-Digo@users.noreply.github.com>
2025-03-19 11:15:01 -0300

    Exemplos da aula 05 de ltpo

---
+++ src/Matriz.java
@@ -0,0 +1,24 @@
+public class Matriz {
+    public static void main(String[] args){
+        int [][] matriz ={
+                {0,1,2},
+                {1,2,3},
+                {2,3,4}
+        };
+
+        for (int[] linha : matriz){
+            for(int valor : linha){
+                System.out.print(valor+"");
+            }
+            System.out.println();
+        }
+
+        int soma = 0;
+        for (int[] linha: matriz){
+            for(int valor : linha){
+                soma += valor;
+            }
+        }
+        System.out.println("Soma:"+soma);
+    }
+}
